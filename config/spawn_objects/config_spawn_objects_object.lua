12f1854dac11179305536928bcff2cd427e40994
Sima Leitch <sima.leitch@gmail.com>
2020-11-16 17:41:02 +0300

    Масштабирование объектов

--- config/spawn_objects/config_spawn_objects_object.lua
+++ config/spawn_objects/config_spawn_objects_object.lua
@@ -1,7 +1,7 @@
 local obj_config = {
     speed_bounds = { 600, 750 },
     sprites_number = 9,
-    d_scale = 0.1
+    d_scale = 0.001
 }
 
 function obj_config:get_speed_bounds() return self.speed_bounds[1], self.speed_bounds[2] end
